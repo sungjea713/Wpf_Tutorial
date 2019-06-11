354852b531209c9c076f637c4035f4c8baa456d3
Sungjea Kim <sungjea713@gmail.com>
2019-06-11 17:11:27 +0900

    Add example: (1)Textblock-string binding, (2)Listview - List/ObervableList binding, (3)Butotn-command binding

--- WpfTutorial/WpfMVVMBasic/Model.cs
+++ WpfTutorial/WpfMVVMBasic/Model.cs
@@ -3,10 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
+using WpfMVVMBasic.MVVM;
 
 namespace WpfMVVMBasic
 {
-    class Model
+    class Model : ObservableObject
     {
+
+        private string bindingMessage;
+
+        public string BindingMessage { get { return bindingMessage; } set { bindingMessage = value; RaisePropertyChanged("BindingMessage"); } }
+        public string CommandBindingMessage { get; set; }
+        public List<string> ListMessage { get; set; } = new List<string>();
+
+        public Model()
+        {
+            BindingMessage = "Binding is done";
+            CommandBindingMessage = "Command binding is done";
+            for(int i = 0; i < 10; i++)
+            {
+                ListMessage.Add(string.Format("[{0}] List binding is done", i));
+            }
+        }
+
     }
 }
--- WpfTutorial/WpfMVVMBasic/ViewModel.cs
+++ WpfTutorial/WpfMVVMBasic/ViewModel.cs
@@ -1,13 +1,62 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using WpfMVVMBasic.MVVM;
+using System.Collections.ObjectModel;
+using System.Threading;
+using System.Windows.Input;
+using System.Windows;
 
 namespace WpfMVVMBasic
 {
-    class ViewModel : ObservableObject
+    public class ViewModel : ObservableObject
     {
+        #region member
+        Model model = new Model();
+        Thread thread;
+        int count = 0;
+        #endregion
+
+        #region Properties
+        public string LabelString { get { return model.BindingMessage; } set { model.BindingMessage = value; RaisePropertyChanged("LabelString"); } }
+        public string ButtonString { get { return model.CommandBindingMessage; } set { model.CommandBindingMessage = value; RaisePropertyChanged("ButtonString"); } }
+        public List<string> ListMessageRaiseMethod { get { return model.ListMessage; } set { model.ListMessage = value; RaisePropertyChanged("ListMessageRaiseMethod"); } }
+        public ObservableCollection<string> ListMessagNonRaise { get; set; } = new ObservableCollection<string>();
+        #endregion
+
+        #region Constructor
+        public ViewModel()
+        {
+            foreach(string str in ListMessageRaiseMethod) ListMessagNonRaise.Add(str);
+   
+            thread = new Thread(new ThreadStart(ThreadMethod));
+            thread.Start();
+            thread.IsBackground = true;
+        }
+        #endregion
+
+        #region Command
+        void ClickExcute()
+        {
+            MessageBox.Show("Button is clicked !");   
+        }
+
+        bool CanClickExcute()
+        {
+            return true;
+        }
+
+        public ICommand Click { get { return new RelayCommand(ClickExcute, CanClickExcute); } }
+        #endregion
+
+        #region Method
+        private void ThreadMethod()
+        {
+            while(true)
+            {
+                LabelString = string.Format("[{0}] binding", count);
+                count++;
+                Thread.Sleep(1000);
+            }
+        }
+        #endregion
     }
 }
