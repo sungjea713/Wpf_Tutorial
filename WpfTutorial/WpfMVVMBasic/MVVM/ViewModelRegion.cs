b3a732f415d123121eeead83213cc1df27a640a3
Sungjea Kim <sungjea713@gmail.com>
2019-06-18 10:28:18 +0900

    Adding JSON project, Adding ViewModel frame

---
+++ WpfTutorial/WpfMVVMBasic/MVVM/ViewModelRegion.cs
@@ -0,0 +1,41 @@
+﻿using System.Windows.Input; //For ICommand
+using System.Collections.ObjectModel; //For ObservableObject
+using WpfMVVMBasic.MVVM;
+
+namespace WpfMVVMBasic.MVVM
+{
+    class ViewModelRegio : ObservableObject
+    {
+        #region member
+
+        #endregion
+
+
+
+        #region constructor
+
+        #endregion
+
+
+
+        #region properties
+
+        #endregion
+
+
+
+        #region commands
+        void TestExcute()
+        {
+
+        }
+        public ICommand Test { get { return new RelayCommand(TestExcute); } }
+        #endregion
+
+
+
+        #region methods
+
+        #endregion
+    }
+}
