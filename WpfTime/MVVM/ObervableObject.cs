767186a4d11b0f4072a2bd867eb68723c95a390f
Sungjea Kim <sungjea713@gmail.com>
2019-07-05 16:30:58 +0900

    ElapsedTime Update

---
+++ WpfTime/MVVM/ObervableObject.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq.Expressions;
+
+namespace WpfTime.MVVM
+{
+    [Serializable]
+    public abstract class ObservableObject : INotifyPropertyChanged
+    {
+        [field: NonSerialized]
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(PropertyChangedEventArgs e)
+        {
+            var handler = this.PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, e);
+            }
+        }
+
+        protected void RaisePropertyChanged<T>(Expression<Func<T>> propertyExpresssion)
+        {
+            var propertyName = PropertySupport.ExtractPropertyName(propertyExpresssion);
+            this.RaisePropertyChanged(propertyName);
+        }
+
+        protected void RaisePropertyChanged(String propertyName)
+        {
+            VerifyPropertyName(propertyName);
+            OnPropertyChanged(new PropertyChangedEventArgs(propertyName));
+        }
+
+        [Conditional("DEBUG")]
+        [DebuggerStepThrough]
+        public void VerifyPropertyName(String propertyName)
+        {
+            // verify that the property name matches a real,  
+            // public, instance property on this Object.
+            if (TypeDescriptor.GetProperties(this)[propertyName] == null)
+            {
+                Debug.Fail("Invalid property name: " + propertyName);
+            }
+        }
+    }
+}
---
+++ WpfTime/ViewModel/ViewModelElapsed.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using WpfTime.MVVM; // for ObservableObject
+using System.Windows.Input; //for ICommand
+using System.Collections.ObjectModel; //for ObservableCollection
+using System.Windows;
+using System.Diagnostics;
+using System.Threading;
+
+namespace WpfTime.ViewModel
+{
+    class ViewModelElapsed : ObservableObject
+    {
+        #region member
+        Thread thClock;
+        string time;
+        TimeSpan currentTime;
+        TimeSpan startTime;
+        #endregion
+
+        #region properties
+        public string Time
+        {
+            get { return time; }
+            set { time = value; RaisePropertyChanged("Time"); }
+        }
+        #endregion
+        #region constructor
+        public ViewModelElapsed()
+        {
+            Time = "00:00:00";
+            startTime = DateTime.Now.TimeOfDay;
+            thClock = new Thread(new ThreadStart(ThreadClock));
+            thClock.IsBackground = true;
+            thClock.Start();
+        }
+        #endregion
+
+        #region command
+
+        #endregion
+
+        #region method
+        private void ThreadClock()
+        {
+            while(true)
+            {
+                currentTime = DateTime.Now.TimeOfDay;
+                TimeSpan elapsedTime = currentTime - startTime;
+                Time = elapsedTime.ToString(@"mm\:ss\:ff");
+                Thread.Sleep(20);
+            }
+        }
+        #endregion
+
+    }
+}
